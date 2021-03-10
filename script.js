bf3afacd1a70c46a32bbbcd5500c98750419053e
Dominika <dominika.jansova1011@gmail.com>
2021-03-10 10:57:12 +0100

    function checkLength and function checkEmail

--- script.js
+++ script.js
@@ -11,17 +11,21 @@ const showError = (input, message) => {
     small.innerText = message;
 }
 
-const showSuccess = (input) => {
+const showSuccess = input => {
     const formStyling = input.parentElement; //div
     formStyling.className = 'form-styling success';
 }
 
-const emailIsValid = (email) => {
+const checkEmail = input => {
         const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return re.test(String(email).toLowerCase());
+        if (re.test(input.value.trim())) {
+            showSuccess(input);
+        } else {
+            showError(input, 'Email is not valid');
+        }
 }
 
-const checkRequired = (inputArr) => {
+const checkRequired = inputArr => {
     inputArr.forEach(function(input) {
         if(input.value.trim() === '') {
             showError(input, `${getFieldName(input)} is required`);
@@ -31,13 +35,26 @@ const checkRequired = (inputArr) => {
     });
 }
 
-const getFieldName = (input) => {
+const getFieldName = input => {
     return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+const checkLength = (input, min, max) => {
+    if(input.value.length < min) {
+        showError(input, `${getFieldName(input)} must be at least ${min} characters`);
+    } else if(input.value.length > max) {
+        showError(input, `${getFieldName(input)} must be less than ${max} characters`);
+    } else {
+        showSuccess(input);
+    }
 }
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
 
     checkRequired([username, email, password, password2]);
+    checkLength(username, 3, 10);
+    checkLength(password, 8, 16);
+    checkEmail(email);
 });
 
